1bc23b47ca3337e014ae6d89d7f367a251bf1c84
rajesh <peketirajesh007@gmail.com>
2023-12-26 10:45:01 +0530

    raje

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Header from '../../components/Header/Header'
 import { FaSearch } from 'react-icons/fa'
+import axios from 'axios'
+import { MdDelete, MdEditSquare } from 'react-icons/md'
 
 const Booking = () => {
     const navigate = useNavigate()
     const { user } = useSelector((user) => user.userReducer)
     const { decision } = useSelector((user) => user.sidebarReducer)
-
+    const [data, setData] = useState([])
+    const [searchedData, setSearchedData] = useState([])
+    const [date, setDate] = useState(new Date(2023, 10, 10))
 
     const navigation = () => {
         if (!user) {
@@ -17,9 +21,39 @@ const Booking = () => {
         }
     }
 
+    const gettingData = async () => {
+        const response = await axios.get('https://hotel-dashboard-w4kx.onrender.com/api/hotels')
+        let arr = []
+        if (response.data) {
+            const result = response.data.data
+            result.forEach((res) => {
+                const next = res.attributes.usersData
+                next.forEach((i) => {
+                    arr.push(i)
+                })
+            })
+        }
+        setData(arr)
+    }
+
+
+
     useEffect(() => {
         navigation()
+        gettingData()
     }, [user])
+
+    const searchhandler = (e) => {
+        const { value } = e.target
+        const res = data.filter((i) => i.name.toLowerCase().includes(value.toLowerCase()))
+        if (value.length >= 1) {
+            setSearchedData(res)
+        }
+        else {
+            setSearchedData([])
+        }
+    }
+
 
     return (
         <>
@@ -55,11 +89,61 @@ const Booking = () => {
                                             <span className='text-gray-800'>All Booking</span>
                                             <div className='bg-white p-2 flex items-center gap-2 text-gray-800'>
                                                 <FaSearch className='' />
-                                                <input type="text" className='outline-none ' />
+                                                <input type="text" className='outline-none ' onChange={searchhandler} />
                                             </div>
                                         </div>
                                         <div className='bg-white py-2 px-4'>
-                                            hi
+                                            <div className='w-full overflow-x-auto flow bg-white'>
+                                                <h2 className='text-gray-700 font-medium'>Booking Details</h2>
+                                                <div className='h-[30rem] overflow-y-scroll min-w-[55rem] flow'>
+                                                    <table className='w-full mt-5'>
+                                                        <tbody>
+                                                            <tr>
+                                                                <th>S.No</th>
+                                                                <th>Name</th>
+                                                                <th>Check In</th>
+                                                                <th>Check Out</th>
+                                                                <th>Status</th>
+                                                                <th>Phone Number</th>
+                                                                <th>Room Type</th>
+                                                                <th>Actions</th>
+                                                            </tr>
+                                                            {
+                                                                searchedData.length ? searchedData && searchedData.map((i, index) => {
+                                                                    return <tr key={index}>
+                                                                        <td>{index + 1}</td>
+                                                                        <td>{i.name}</td>
+                                                                        <td>{`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`}</td>
+                                                                        <td>{`${date.getDate() + Math.floor(Math.random() * 3)}-${date.getMonth() + 1}-${date.getFullYear()}`}</td>
+                                                                        <td>{i.paidStatus ? <span className='text-green-700 border p-1 rounded-md border-green-700 text-sm'>Paid</span> : <span className='text-red-600 border p-1 rounded-md border-red-600 text-xs'>Not Paid</span>}</td>
+                                                                        <td>{i.phoneNumber}</td>
+                                                                        <td>{i.roomType}</td>
+                                                                        <td className='text-xl'>
+                                                                            <MdEditSquare className='inline me-4 text-blue-500' />
+                                                                            <MdDelete className='inline text-red-700 cursor-pointer' />
+                                                                        </td>
+                                                                    </tr>
+                                                                }) :
+                                                                    data && data.map((i, index) => {
+                                                                        return <tr key={index}>
+                                                                            <td>{index + 1}</td>
+                                                                            <td>{i.name}</td>
+                                                                            <td>{`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`}</td>
+                                                                            <td>{`${date.getDate() + Math.floor(Math.random() * 3)}-${date.getMonth() + 1}-${date.getFullYear()}`}</td>
+                                                                            <td>{i.paidStatus ? <span className='text-green-700 border p-1 rounded-md border-green-700 text-sm'>Paid</span> : <span className='text-red-600 border p-1 rounded-md border-red-600 text-xs'>Not Paid</span>}</td>
+                                                                            <td>{i.phoneNumber}</td>
+                                                                            <td>{i.roomType}</td>
+                                                                            <td className='text-xl'>
+                                                                                <MdEditSquare className='inline me-4 text-blue-500' />
+                                                                                <MdDelete className='inline text-red-700 cursor-pointer' />
+                                                                            </td>
+                                                                        </tr>
+                                                                    })
+                                                            }
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
                                         </div>
                                     </section>
                                 </div>
